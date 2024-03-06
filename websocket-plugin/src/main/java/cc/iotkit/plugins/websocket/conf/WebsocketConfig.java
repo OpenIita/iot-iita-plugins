f631bbfd8aa7cf3e6bbb7d0fe0224ce5adb58ea4
tangfudong <280620913@qq.com>
2024-03-06 09:25:57 +0800

    1、添加websocket插件包

---
+++ websocket-plugin/src/main/java/cc/iotkit/plugins/websocket/conf/WebsocketConfig.java
@@ -0,0 +1,39 @@
+/*
+ * +----------------------------------------------------------------------
+ * | Copyright (c) 奇特物联 2021-2022 All rights reserved.
+ * +----------------------------------------------------------------------
+ * | Licensed 未经许可不能去掉「奇特物联」相关版权
+ * +----------------------------------------------------------------------
+ * | Author: xw2sy@163.com
+ * +----------------------------------------------------------------------
+ */
+package cc.iotkit.plugins.websocket.conf;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "websocket")
+public class WebsocketConfig {
+
+    private int port;
+
+    private String sslKey;
+
+    private String sslCert;
+
+    private boolean ssl;
+
+    private List<AccessToken> accessTokens;
+
+    @Data
+    public static class AccessToken{
+        private String tokenName;
+        private String tokenStr;
+    }
+
+}
